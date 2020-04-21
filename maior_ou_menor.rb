1abe7d50107302d362fce58a671cd5f018fb2aa4
Rafael <faelleonan@gmail.com>
2020-04-21 15:04:43 -0300

    Fluxo condicional

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -2,10 +2,7 @@ puts "Bem vindo ao jogo da adivinhação!"
 puts "Qual é o seu nome?"
 nome = gets
 
-puts 
-puts 
-puts
-puts
+puts "\n\n\n\n"
 
 puts "Começaremos o jogo para você, "+ nome
 
@@ -14,14 +11,15 @@ numero_secreto = 175
 
 puts "Ecolhido.. que tal adivinhar o nosso número secreto?"
 
-puts 
-puts
-puts
-puts
+puts "\n\n\n\n"
 
 puts "Tentativa 1"
 puts "Escolha um número: "
 chute = gets
 
 puts "Será que acertou ? Você chutou "+chute
-puts chute.to_i == numero_secreto
+if chute.to_i == numero_secreto
+	puts "Acertou !"
+else 
+	puts "Errou!"
+end
